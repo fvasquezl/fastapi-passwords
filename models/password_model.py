88b6c94c6aa09b19ba22916ec8f3ebcb34772575
Faustino Vasquez <vasquez.limon@gmail.com>
2024-04-08 23:34:23 -0700

    models

--- models/password_model.py
+++ models/password_model.py
@@ -1,19 +1,38 @@
-from sqlalchemy import Column, ForeignKey, Integer, String
+from typing import List
+from sqlalchemy import Column, ForeignKey, Integer, String, Table
 from sqlalchemy.orm import relationship
 from config.database import Base
+from sqlalchemy.orm import Mapped
+from sqlalchemy.orm import mapped_column
+from .category_model import CategoryDB
+from .user_model import UserDB
+from .tag_model import TagDB
+
+
+association_table = Table(
+    "association",
+    Base.metadata,
+    Column("password_id", Integer, ForeignKey("passwords.id")),
+    Column("tag_id", Integer, ForeignKey("tags.id")),
+)
 
 
 class PasswordDB(Base):
     __tablename__ = "passwords"
 
-    id = Column(Integer, primary_key=True, index=True)
-    name = Column(String(45))
-    slug = Column(String(45))
-    extract = Column(String(45))
-    body = Column(String(45))
-    status = Column(Integer)
-    category_id = Column(Integer, ForeignKey("categories.id"))
-    user_id = Column(Integer, ForeignKey("users.id"))
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, index=True)
+    name: Mapped[str] = mapped_column(len(45))
+    slug: Mapped[str] = mapped_column(len(45))
+    extract: Mapped[str] = mapped_column(len(45))
+    body: Mapped[str] = mapped_column(len(45))
+    status: Mapped[int]
+    category_id: Mapped[int] = mapped_column(Integer, ForeignKey("categories.id"))
+    user_id: Mapped[int] = mapped_column(Integer, ForeignKey("users.id"))
 
-    category = relationship("CategoryDB", back_populates="passwords")
-    user = relationship("UserDB", back_populates="passwords")
+    category: Mapped["CategoryDB"] = relationship(back_populates="passwords")
+
+    user: Mapped["UserDB"] = relationship(back_populates="passwords")
+
+    tags: Mapped[List[TagDB]] = relationship(
+        secondary=association_table, back_populates="passwords"
+    )
--- models/tag_model.py
+++ models/tag_model.py
@@ -1,10 +1,19 @@
+from typing import List
 from sqlalchemy import Column, Integer, String
 from config.database import Base
+from sqlalchemy.orm import relationship, Mapped
+from sqlalchemy.orm import mapped_column
+
+from .password_model import PasswordDB, association_table
 
 
 class TagDB(Base):
     __tablename__ = "tags"
 
-    id = Column(Integer, primary_key=True, index=True)
-    name = Column(String(45))
-    slug = Column(String(45))
+    id: Mapped[int] = mapped_column(primary_key=True, index=True)
+    name: Mapped[str] = mapped_column(len(45))
+    slug: Mapped[str] = mapped_column(len(45))
+
+    passwords = Mapped[List[PasswordDB]] = relationship(
+        secondary=association_table, back_populates="tags"
+    )
